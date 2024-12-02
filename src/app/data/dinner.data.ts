3005f404fead284afbd4c1e088de76dc9f122975
Jackson E <lavaskin@gmail.com>
2024-12-01 19:39:28 -0800

    add data for dinner tourney

--- src/app/data/dinner.data.ts
+++ src/app/data/dinner.data.ts
@@ -3,44 +3,132 @@ import { Contestant } from "../models/contestant.model";
 const dinnerContestants: Contestant[] = [
 	{
 		name: 'Lasagna',
-		thumbnailUrl: '',
+		thumbnailUrl: 'lasagna.jpg',
 	},
 	{
 		name: 'Mac & Cheese',
-		thumbnailUrl: '',
+		thumbnailUrl: 'mac_cheese.jpg',
 	},
 	{
 		name: 'Meatloaf',
-		thumbnailUrl: '',
+		thumbnailUrl: 'meatloaf.jpg',
 	},
 	{
 		name: 'Pad Thai',
-		thumbnailUrl: '',
+		thumbnailUrl: 'pad_thai.jpg',
 	},
 	{
 		name: 'Pizza',
-		thumbnailUrl: '',
+		thumbnailUrl: 'pizza.jpg',
 	},
 	{
 		name: 'Fettachini Alfredo',
-		thumbnailUrl: '',
+		thumbnailUrl: 'fettucinie_alfredo.jpg',
 	},
 	{
 		name: 'Burrito',
-		thumbnailUrl: '',
+		thumbnailUrl: 'burrito.jpg',
 	},
 	{
 		name: 'Mashed Potatoes',
-		thumbnailUrl: '',
+		thumbnailUrl: 'mashed_potatoes.jpg',
 	},
-
-	/*
 	{
-		name: '',
-		thumbnailUrl: '',
-		weight: 1,
+		name: 'Mac & Cheese Casserole',
+		thumbnailUrl: 'macncheese_caserole.jpg',
 	},
-	*/
+	{
+		name: 'Sushi',
+		thumbnailUrl: 'sushi.jpg',
+	},
+	{
+		name: 'Fried Chicken',
+		thumbnailUrl: 'fried_chicken.jpg',
+	},
+	{
+		name: 'Burger',
+		thumbnailUrl: 'burger.jpg',
+	},
+	{
+		name: 'Steak',
+		thumbnailUrl: 'steak.jpg',
+	},
+	{
+		name: 'Ramen',
+		thumbnailUrl: 'ramen.jpg',
+	},
+	{
+		name: 'Curry',
+		thumbnailUrl: 'curry.jpg',
+	},
+	{
+		name: 'Clam Chowder',
+		thumbnailUrl: 'clam_chowder.jpg',
+	},
+	{
+		name: 'Spaghetti',
+		thumbnailUrl: 'spaghetti.jpg',
+	},
+	{
+		name: 'Brisket',
+		thumbnailUrl: 'brisket.jpg',
+	},
+	{
+		name: "Shepherd's Pie",
+		thumbnailUrl: 'shepards_pie.jpg',
+	},
+	{
+		name: 'Chili Dog',
+		thumbnailUrl: 'chili_dog.jpg',
+	},
+	{
+		name: 'Swedish Meatballs',
+		thumbnailUrl: 'meatballs.jpg',
+	},
+	{
+		name: 'Grilled Cheese & Tomato Soup',
+		thumbnailUrl: 'grilled_cheese_tomato_soup.jpg',
+	},
+	{
+		name: 'Chili Rice',
+		thumbnailUrl: 'chili_rice.jpg',
+	},
+	{
+		name: 'Chicken Burger',
+		thumbnailUrl: 'chicken_burger.jpg',
+	},
+	{
+		name: 'Turkey Club',
+		thumbnailUrl: 'turkey_club.jpg',
+	},
+	{
+		name: 'Cream of Potato',
+		thumbnailUrl: 'cream_of_potato.jpg',
+	},
+	{
+		name: 'Chicken Breast',
+		thumbnailUrl: 'chicken_breast.jpg',
+	},
+	{
+		name: 'BBQ Ribs',
+		thumbnailUrl: 'bbq_ribs.jpg',
+	},
+	{
+		name: 'Boneless Wings',
+		thumbnailUrl: 'boneless_wings.jpg',
+	},
+	{
+		name: 'Bone-in Wings',
+		thumbnailUrl: 'bonein_wings.jpg',
+	},
+	{
+		name: 'Enchiladas',
+		thumbnailUrl: 'enchalada.jpg',
+	},
+	{
+		name: 'Gyro',
+		thumbnailUrl: 'gyro.jpg',
+	},
 ];
 
 export default dinnerContestants;
