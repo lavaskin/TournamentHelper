62b387c7b6dbfc1f9368fed789b85350f2c01a20
Jackson E <lavaskin@gmail.com>
2025-02-15 17:08:48 -0800

    add titles to tournaments

--- src/app/data/tournaments.data.ts
+++ src/app/data/tournaments.data.ts
@@ -5,24 +5,28 @@ import posterContestants from "./posters.data";
 import waifuContestants from "./waifus.data";
 
 const animalTournamnent: Tournament = {
+	title: "Animal 1v1's",
 	contestants: animalContestants,
 	thumbnailPath: 'animals',
 	shuffle: true,
 }
 
 const dinnerTournament: Tournament = {
+	title: "Dinner Tournament",
 	contestants: dinnerContestants,
 	thumbnailPath: 'dinner',
 	shuffle: true,
 }
 
 const posterTournament: Tournament = {
+	title: "Movie Poster Tournament",
 	contestants: posterContestants,
 	thumbnailPath: 'poster',
 	shuffle: true,
 }
 
 const waifuTournament: Tournament = {
+	title: "Ultimate Waifu Tournament",
 	contestants: waifuContestants,
 	thumbnailPath: 'waifus',
 	shuffle: true,
--- src/app/models/tournament.model.ts
+++ src/app/models/tournament.model.ts
@@ -1,6 +1,7 @@
 import { Contestant } from "./contestant.model";
 
 export interface Tournament {
+	title: string,
 	contestants: Contestant[],
 	thumbnailPath: string,
 	shuffle?: boolean,
