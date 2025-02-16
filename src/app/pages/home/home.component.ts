26a8118cf991caf560c6b950510321a885f91bfd
Jackson E <lavaskin@gmail.com>
2025-02-15 17:09:18 -0800

    add extremely basic home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Tournament } from '../../models/tournament.model';
+import { animalTournamnent, dinnerTournament, posterTournament, waifuTournament } from '../../data/tournaments.data';
+import { TournamentService } from '../../services/tournament.service';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'page-home',
@@ -9,5 +13,18 @@ import { CommonModule } from '@angular/common';
 	styleUrl: './home.component.css'
 })
 export class HomeComponent {
+	public tournaments: Tournament[] = [
+		posterTournament,
+		animalTournamnent,
+		dinnerTournament,
+		waifuTournament,
+	];
 
+	private _tService: TournamentService = inject(TournamentService);
+	private _router: Router = inject(Router);
+
+	public selectTournament(selected: Tournament): void {
+		this._tService.setTournament(selected);
+		this._router.navigate(['/tournament']);
+	};
 }
