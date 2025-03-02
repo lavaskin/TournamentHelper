6f1665b4fdedb5b1e2cab456c0c1116afd5f5bb9
Jackson E <lavaskin@gmail.com>
2025-03-01 22:27:48 -0800

    add waifu tourney data

--- src/app/data/waifus.data.ts
+++ src/app/data/waifus.data.ts
@@ -1,7 +1,281 @@
 import { Contestant } from "../models/contestant.model";
 
 const waifuContestants: Contestant[] = [
+	// Pokemon (https://www.youtube.com/watch?v=KgG_kUF7Zpk)
+	{
+		name: 'Leaf (Pokemon)',
+		thumbnailUrl: 'pokemon/leaf.png',
+	},
+	{
+		name: 'Elesa (Pokemon)',
+		thumbnailUrl: 'pokemon/elesa.png',
+	},
+	{
+		name: 'Marine (Pokemon)',
+		thumbnailUrl: 'pokemon/marine.png',
+	},
+	{
+		name: 'Misty (Pokemon)',
+		thumbnailUrl: 'pokemon/misty.png',
+	},
+	{
+		name: 'Rosa (Pokemon)',
+		thumbnailUrl: 'pokemon/rosa.png',
+	},
+	{
+		name: 'Mallow (Pokemon)',
+		thumbnailUrl: 'pokemon/mallow.png',
+	},
+	{
+		name: 'Nessa (Pokemon)',
+		thumbnailUrl: 'pokemon/nessa.png',
+	},
+	{
+		name: 'Dawn (Pokemon)',
+		thumbnailUrl: 'pokemon/dawn.png',
+	},
+	{
+		name: 'Sonia (Pokemon)',
+		thumbnailUrl: 'pokemon/sonia.png',
+	},
 
+	// Disney (https://www.youtube.com/watch?v=Y3XcNUiOfXA
+	{
+		name: 'GoGo Tomago (Big Hero 6)',
+		thumbnailUrl: 'disney/tomago.png',
+	},
+	{
+		name: 'Megara (Hercules)',
+		thumbnailUrl: 'disney/megara.png',
+	},
+	{
+		name: 'Shank (Ralph Breaks the Internet)',
+		thumbnailUrl: 'disney/shank.png',
+	},
+	{
+		name: 'Tiana (The Princess and the Frog)',
+		thumbnailUrl: 'disney/tiana.png',
+	},
+	{
+		name: 'Kida (Atlantis)',
+		thumbnailUrl: 'disney/kida.png',
+	},
+
+	// Genshin (https://www.youtube.com/watch?v=GDhdrltVhfs)
+	{
+		name: 'Yae Miko (Genshin Impact)',
+		thumbnailUrl: 'genshin/yae.png',
+	},
+	{
+		name: 'Hu Tao (Genshin Impact)',
+		thumbnailUrl: 'genshin/hutao.png',
+	},
+	{
+		name: 'Amber (Genshin Impact)',
+		thumbnailUrl: 'genshin/amber.png',
+	},
+	{
+		name: 'Raiden Shogun (Genshin Impact)',
+		thumbnailUrl: 'genshin/raiden.png',
+	},
+	{
+		name: 'Yelan (Genshin Impact)',
+		thumbnailUrl: 'genshin/yelan.png',
+	},
+
+	// Battle Cats (https://www.youtube.com/watch?v=HXd9V7wRpDg)
+	{
+		name: 'Mina (Battle Cats)',
+		thumbnailUrl: 'battlecats/yelan.png',
+	},
+	{
+		name: 'Twinstars (Battle Cats)',
+		thumbnailUrl: 'battlecats/twinstars.png',
+	},
+	{
+		name: 'Kasli (Battle Cats)',
+		thumbnailUrl: 'battlecats/kasli.png',
+	},
+	{
+		name: 'Iz (Battle Cats)',
+		thumbnailUrl: 'battlecats/iz.png',
+	},
+	{
+		name: 'Years End Neneko (Battle Cats)',
+		thumbnailUrl: 'battlecats/neneko.png',
+	},
+
+	// Traps (https://www.youtube.com/watch?v=OiXigOktirg)
+	{
+		name: 'Zero (Drakengard 3)',
+		thumbnailUrl: 'traps/zero.png',
+	},
+	{
+		name: 'Furina (Genshin Impact)',
+		thumbnailUrl: 'traps/furina.png',
+	},
+	{
+		name: 'Beidou (Genshin Impact)',
+		thumbnailUrl: 'traps/beidou.png',
+	},
+	{
+		name: 'Bridget (Guilty Gear)',
+		thumbnailUrl: 'traps/bridget.png',
+	},
+	{
+		name: 'Moxxi (Borderlands)',
+		thumbnailUrl: 'traps/moxxi.png',
+	},
+	{
+		name: 'Gardevoir (Pokemon)',
+		thumbnailUrl: 'traps/gardevoir.png',
+	},
+	{
+		name: 'Load Galax (Gatchaman Crowds)',
+		thumbnailUrl: 'traps/galax.png',
+	},
+	{
+		name: 'Yuri Zahard (Tower of God)',
+		thumbnailUrl: 'traps/yuri.png',
+	},
+
+	// Final Fantasy (https://www.youtube.com/watch?v=SwUAQh6M3wI)
+	{
+		name: 'Faris (FF5)',
+		thumbnailUrl: 'ff/faris.png',
+	},
+	{
+		name: 'Celes (FF6)',
+		thumbnailUrl: 'ff/celes.png',
+	},
+	{
+		name: 'Tifa (FF7)',
+		thumbnailUrl: 'ff/tifa.png',
+	},
+	{
+		name: 'Yuffie (FF7)',
+		thumbnailUrl: 'ff/yuffie.png',
+	},
+	{
+		name: 'Lulu (FF10)',
+		thumbnailUrl: 'ff/lulu.png',
+	},
+	{
+		name: 'Vanille (FF)',
+		thumbnailUrl: 'ff/vanille.png',
+	},
+	{
+		name: "Y'Shtola (FF14)",
+		thumbnailUrl: 'ff/yshtola.png',
+	},
+	{
+		name: 'Minfilia (FF14)',
+		thumbnailUrl: 'ff/minfilia.png',
+	},
+	{
+		name: 'Benedikta (FF16)',
+		thumbnailUrl: 'ff/benedikta.png',
+	},
+
+	// Cartoon Moms (https://www.youtube.com/watch?v=jDNTmvoCerY)
+	{
+		name: 'Nicole (The Amazing World of Gumball)',
+		thumbnailUrl: 'cartoons/nicole.png',
+	},
+	{
+		name: 'Mavis (Hotel Transylvania)',
+		thumbnailUrl: 'cartoons/mavis.png',
+	},
+
+	// Hoyoverse (https://www.youtube.com/watch?v=I7dltxP976Y)
+	{
+		name: 'Arlecchino (Genshin Impact)',
+		thumbnailUrl: 'hoyoverse/arle.png',
+	},
+	{
+		name: 'Herrcher of Thunder (HI3)',
+		thumbnailUrl: 'hoyoverse/thunder.png',
+	},
+	{
+		name: "Schicksal's Imperative (Honkai Impact 3rd)",
+		thumbnailUrl: 'hoyoverse/schicksal.png',
+	},
+	{
+		name: 'Sparkle (Honkai Star Rail)',
+		thumbnailUrl: 'hoyoverse/sparkle.png',
+	},
+	{
+		name: 'Soukaku (Zenless Zone Zero)',
+		thumbnailUrl: 'hoyoverse/soukaku.png',
+	},
+	{
+		name: 'Miyabi (Zenless Zone Zero)',
+		thumbnailUrl: 'hoyoverse/miyabi.png',
+	},
+	{
+		name: 'Burnice (Zenless Zone Zero)',
+		thumbnailUrl: 'hoyoverse/burnice.png',
+	},
+
+	// League of Legends (https://www.youtube.com/watch?v=Cjm6thhYNbQ)
+	{
+		name: 'Ahri (League of Legends)',
+		thumbnailUrl: 'lol/ahri.png',
+	},
+	{
+		name: 'Gwen (League of Legends)',
+		thumbnailUrl: 'lol/gwen.png',
+	},
+	{
+		name: 'Neeko (League of Legends)',
+		thumbnailUrl: 'lol/neeko.png',
+	},
+	{
+		name: 'Xayah (League of Legends)',
+		thumbnailUrl: 'lol/xayah.png',
+	},
+
+	// Favorite Anime (https://www.youtube.com/watch?v=zgDzCHSxWOs)
+	{
+		name: 'Daki (Demon Slayer)',
+		thumbnailUrl: 'anime/daki.png',
+	},
+	{
+		name: 'Nezuko (Demon Slayer)',
+		thumbnailUrl: 'anime/nezuko.png',
+	},
+	{
+		name: 'Esdeath (Akame ga Kill)',
+		thumbnailUrl: 'anime/esdeath.png',
+	},
+	{
+		name: 'Historia (Attack on Titan)',
+		thumbnailUrl: 'anime/historia.png',
+	},
+	{
+		name: 'Frieda (Attack on Titan)',
+		thumbnailUrl: 'anime/frieda.png',
+	},
+	{
+		name: 'Kyou (Clannad)',
+		thumbnailUrl: 'anime/kyou.png',
+	},
+	{
+		name: 'Misae (Clannad)',
+		thumbnailUrl: 'anime/misae.png',
+	},
+	{
+		name: 'Sanae (Clannad)',
+		thumbnailUrl: 'anime/sanae.png',
+	},
+	{
+		name: 'Ryuuko (Kill la Kill)',
+		thumbnailUrl: 'anime/ryuuko.png',
+	},
+	{
+		name: 'Nonon (Kill la Kill)',
+		thumbnailUrl: 'anime/nonon.png',
+	},
 ];
 
 export default waifuContestants;
