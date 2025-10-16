8e2ad8cf4570fd3d0a8fafacb8a47ef10804a6c3
lavaskin@gmail.com <lavaskin@gmail.com>
2025-10-16 14:20:33 -0700

    flesh out albums tournament

--- src/app/data/album-covers.data.ts
+++ src/app/data/album-covers.data.ts
@@ -1,7 +1,134 @@
 import { Contestant } from "../models/contestant.model";
 
 const albumCoverContestants: Contestant[] = [
-	// ...
+	{
+		name: "Costmic Warfare (2023)",
+		thumbnailUrl: "Atavistia_Costmic Warfare_2023.png",
+	},
+	{
+		name: "Stygian (2020)",
+		thumbnailUrl: "Atramentus_Stygian_2020.png",
+	},
+	{
+		name: "Shrine (2022)",
+		thumbnailUrl: "Bleed from Within_Shrine_2022.png",
+	},
+	{
+		name: "Brat (2024)",
+		thumbnailUrl: "Charli XCX_Brat_2024.png",
+	},
+	{
+		name: "She's So Unusual (1983)",
+		thumbnailUrl: "Cyndi Lauper_She's So Unusual_1983.png",
+	},
+	{
+		name: "Elvis Presley (1956)",
+		thumbnailUrl: "Elvis Presley_Elvis Presley_1956.png",
+	},
+	{
+		name: "The Now Now (2018)",
+		thumbnailUrl: "Gorillaz_The Now Now_2018.png",
+	},
+	{
+		name: "Live Through This (1994)",
+		thumbnailUrl: "Hole_Live Through This_1994.png",
+	},
+	{
+		name: "Smoke + Mirrors (2015)",
+		thumbnailUrl: "Imagine Dragons_Smoke + Mirrors_2015.png",
+	},
+	{
+		name: "Unknown Pleasures (1979)",
+		thumbnailUrl: "Joy Devision_Unknown Pleasures_1979.png",
+	},
+	{
+		name: "British Steel (1980)",
+		thumbnailUrl: "Judas Priest_British Steel_1980.png",
+	},
+	{
+		name: "Damn (2017)",
+		thumbnailUrl: "Kendrick Lamar_Damn_2017.png",
+	},
+	{
+		name: "To Pimp a Butterfly (2015)",
+		thumbnailUrl: "Kendrick Lamar_To Pimp a Butterfly_2015.png",
+	},
+	{
+		name: "Led Zeppelin (1969)",
+		thumbnailUrl: "Led Zeppelin_Led Zeppelin_1969.png",
+	},
+	{
+		name: "Master of Puppets (1986)",
+		thumbnailUrl: "Metallica_Master of Puppets_1986.png",
+	},
+	{
+		name: "Use Your Illusion (1991)",
+		thumbnailUrl: "Guns N' Roses_Use Your Illusion_1991.png",
+	},
+	{
+		name: "Straight Outta Compton (1988)",
+		thumbnailUrl: "N.W.A_Straight Outta Compton_1988.png",
+	},
+	{
+		name: "Illmatic (1994)",
+		thumbnailUrl: "Nas_Illmatic_1994.png",
+	},
+	{
+		name: "Urn (2017)",
+		thumbnailUrl: "Ne Obliviscaris_Urn_2017.png",
+	},
+	{
+		name: "Nevermind (1991)",
+		thumbnailUrl: "Nirvana_Nevermind_1991.png",
+	},
+	{
+		name: "Human (2021)",
+		thumbnailUrl: "OneRepublic_Human_2021.png",
+	},
+	{
+		name: "Horses (1975)",
+		thumbnailUrl: "Patti Smith_Horses_1975.png",
+	},
+	{
+		name: "The Dark Side of the Moon (1973)",
+		thumbnailUrl: "Pink Floyd_The Dark Side of the Moon_1973.png",
+	},
+	{
+		name: "Fear of a Black Planet (1990)",
+		thumbnailUrl: "Public Enemy_Fear of a Black Planet_1990.png",
+	},
+	{
+		name: "Where Owls Know My Name (2018)",
+		thumbnailUrl: "Rivers of Nihil_Where Owls Know My Name_2018.png",
+	},
+	{
+		name: "Abbey_Road (1969)",
+		thumbnailUrl: "The Beatles_Abbey_Road_1969.png",
+	},
+	{
+		name: "Sgt. Pepper's Lonely Hearts Club Band (1967)",
+		thumbnailUrl: "The Beatles_Sgt Pepper’s Lonely Hearts Club Band_1967.png",
+	},
+	{
+		name: "Ready to Die (1994)",
+		thumbnailUrl: "The Notorious B.I.G._Ready to Die_1994.png",
+	},
+	{
+		name: "Under The Pressure (2019)",
+		thumbnailUrl: "The Score_Under The Pressure_2019.png",
+	},
+	{
+		name: "The Velvet Underground and Nico (1967)",
+		thumbnailUrl: "The Velvet Underground and Nico_The Velvet Underground and Nico_1967.png",
+	},
+	{
+		name: "Free Verse (2024)",
+		thumbnailUrl: "UKRampage_Free Verse_2024.png",
+	},
+	{
+		name: "That's Why I Gave Up On Music (2019)",
+		thumbnailUrl: "Yorushika_That's Why I Gave Up On Music_2019.png",
+	},
 ];
 
 export default albumCoverContestants;
